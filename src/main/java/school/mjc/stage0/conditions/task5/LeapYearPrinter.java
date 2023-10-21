59f18606260b54603ca29a4e38fd5dc629924811
Нодир Асомов <nasomov93@gmail.com>
2023-10-21 18:23:04 +0500

    hi

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYear;
+        isLeapYear =(year % 4 == 0);
+
+        isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+        if (isLeapYear) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter  leapYearPrinter =new LeapYearPrinter();
+        leapYearPrinter.isLeapYear(2024);
     }
 }
+
--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+
+
+        switch (1 <= number && number <= 5  ? 0 : (6 <= number && number <= 10) ? 1 : 2) {
+
+            case 0:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 1:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 2:
+                System.out.println("out of range");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        RangeNumbersPrinter rangeNumbersPrinter = new RangeNumbersPrinter();
+        rangeNumbersPrinter.printRange(11);
     }
 }
